178c802ffd380f4b5972bd41521f6003404839a9
Aleksei <headofhell@gmail.com>
2025-12-03 04:29:17 +0400

    fix

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_PIXEL_DENSITY = 10;
-export const MAX_DOWNSAMPLE_WIDTH  = 432;
-export const MAX_DOWNSAMPLE_HEIGHT = 768;
+export const MAX_DOWNSAMPLE_WIDTH  = 768;
+export const MAX_DOWNSAMPLE_HEIGHT = 432;
 export const DEFAULT_MC_VERSION = "1.21.1";
